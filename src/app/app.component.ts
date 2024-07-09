737bc9642753198a1dd7ecfca5ab6fd312d57d05
Hasim <shasim148@gmail.com>
2024-07-09 18:53:08 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-
+import { pdfDefaultOptions } from 'ngx-extended-pdf-viewer';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ngx-pdf-editor';
+  filenameForDownload : string = "";
 }
